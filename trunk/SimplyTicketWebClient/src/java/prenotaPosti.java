d553bade37bc6ab203c02841a1b8f3260f1fbd68
rafspiny <rafspiny@1199cade-bc2d-11de-b52b-63f31fbada87>
2010-02-04 00:16:49 +0000

    Added multiple ticket sold/refuse

--- trunk/SimplyTicketWebClient/src/java/prenotaPosti.java
+++ trunk/SimplyTicketWebClient/src/java/prenotaPosti.java
@@ -10,6 +10,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
@@ -19,6 +20,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.*;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -53,8 +57,8 @@ public class prenotaPosti extends HttpServlet {
             Logger.getLogger(loginServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-   
-    /** 
+
+    /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
@@ -76,8 +80,6 @@ public class prenotaPosti extends HttpServlet {
         catch (NumberFormatException e) {
             throw new ServletException("Addetto non valido",e);
         }
-        String[] prenotazione=request.getParameterValues("prenota");
-        String[] rimborso=request.getParameterValues("rimborso");
         double intero;
         double ridotto;
         try {
@@ -87,130 +89,141 @@ public class prenotaPosti extends HttpServlet {
         catch (NumberFormatException e) {
             throw new ServletException("Prezzi non validi");
         }
-        //name=\"tipo_biglietto\" value=\"intero\"
         String tipoBiglietto=request.getParameter("tipo_biglietto");
         if (tipoBiglietto==null || tipoBiglietto.equals("")) {
             throw new ServletException("Tipo biglietto non selezionato");
         }
         String proiezione = request.getParameter("idProiezione");
-        String operazione = request.getParameter("prenota");
-        String sceltaFila=request.getParameter("sceltaFila");
-        String sceltaPosto=request.getParameter("sceltaPosto");
-        operazione.trim();
         proiezione=proiezione.trim();
         if (proiezione==null || proiezione.equals("")) {
-            throw new ServletException("Proiez non valida"+proiezione+"xx"+operazione+"xx"+sceltaFila);
+            throw new ServletException("Proiezione non valida"+proiezione);
         }
-        int posto,fila;
-        
-        try {
-            posto=Integer.parseInt(sceltaPosto);
-            fila=Integer.parseInt(sceltaFila);
-        }
-        catch(NumberFormatException e) {
-            throw new ServletException("Parametri della prenotazione non corretti", e);
-        }
-        if (operazione!=null && operazione.equalsIgnoreCase("Prenota")) {
-              if (posto>0 && fila>0) {
-                    Collezione postiRitornati=null;
-                    String[] postoSuCuiLavorare=null;
-                    try {
-                      postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,posto);
-                      postoSuCuiLavorare = ( (String[]) postiRitornati.getIndex(0));
-                    }
-                    catch (java.rmi.ConnectException e) {
-                        this.initialize();
+        String value=request.getParameter("dati");
+        if (value!=null && !value.equals("")) {
+            try {
+                JSONParser parser = new JSONParser();
+                Object obj = parser.parse(value);
+                JSONObject root = (JSONObject) obj;
+                JSONObject lista = (JSONObject) root.get("lista");
+                JSONArray prenota = (JSONArray)(lista.get("prenota"));
+                JSONObject posto=null;
+                int fila=0,id=0;
+                for (Iterator i = prenota.iterator(); i.hasNext();) {
+                    posto = (JSONObject)i.next();
+                    posto=(JSONObject) posto.get("posto");
+                    fila=Integer.parseInt((String)(posto.get("fila")));
+                    id=Integer.parseInt((String)(posto.get("id")));
+//                    out.println("Prenota: "+posto.get("proiezione")+" "+posto.get("id")+" "+posto.get("fila")+"<br>\n");
+                    if (id>0 && fila>0) {
+                        Collezione postiRitornati=null;
+                        String[] postoSuCuiLavorare=null;
                         try {
-                            postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,posto);
+                            postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,id);
                             postoSuCuiLavorare = ( (String[]) postiRitornati.getIndex(0));
-                        } catch (Exception ex) {
-                            throw new ServletException("Errore nella comunicazione con il server SimplyTicket", ex);
-                        }
-                    }
-                    catch (Exception e1) {
-                      throw new ServletException("Errore nella comunicazione con il server SimplyTicket", e1);
-                    }
-                    if (postoSuCuiLavorare[2].equals("TRUE")) {
-                        //JOptionPane.showMessageDialog(null,"            PostoOccupato","Errore",JOptionPane.ERROR_MESSAGE);
-                    }
-                    else {
-
-                      if (tipoBiglietto.equalsIgnoreCase("intero")) {
-                        ridotto=0;
-                      }
-                      else {
-                        intero=0;
-                      }
-                      try {
-                        controllerBiglietteria.emettiTicket(proiezione, fila, posto, "no",intero,ridotto,IDAddetto);
-                      }
-                      catch (java.rmi.ConnectException e) {
-                          this.initialize();
-                          try {
-                              controllerBiglietteria.emettiTicket(proiezione, fila, posto, "no",intero,ridotto,IDAddetto);
-                          } catch (Exception ex) {
-                              throw new ServletException("Errore Emissione Ticket", ex);
-                          }
-                      }
-                      catch (Exception ex) {
-                          throw new ServletException("Errore Emissione Ticket", ex);
-                      }
-                    }
-                }
-                else {
-                    throw new ServletException("Non hai selezionato alcun posto");
-                }
-        }
-        else {
-            if (operazione!=null && operazione.equalsIgnoreCase("Rimborsa")) {
-                if (posto>0 && fila>0) {
-                    Collezione postiRitornati=null;
-                    String[] postoSuCuiLavorare=null;
-                    try {
-                        postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,posto);
-                        postoSuCuiLavorare = ( (String[]) postiRitornati.getIndex(0));
-                    }
-                    catch (java.rmi.ConnectException e) {
-                        this.initialize();
-                        try {
-                            postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,posto);
-                            postoSuCuiLavorare = ( (String[]) postiRitornati.getIndex(0));
-                        } catch (Exception ex) {
-                            throw new ServletException("Errore nella comunicazione con il server SimplyTicket", ex);
-                        }
-                    }
-                    catch (Exception e1) {
-                        throw new ServletException("Errore nella comunicazione con il server SimplyTicket", e1);
-                    }
-                    if (postoSuCuiLavorare[2].equals("FALSE")) {
-                                //JOptionPane.showMessageDialog(null,"            PostoOccupato","Errore",JOptionPane.ERROR_MESSAGE);
-                    }
-                    else {
-                        if (tipoBiglietto.equalsIgnoreCase("intero")) {
-                            ridotto=0;
-                        }
-                        else {
-                            intero=0;
-                        }
-                        try {
-                            controllerBiglietteria.annullaTicket(proiezione, posto, fila, "no");
                         }
                         catch (java.rmi.ConnectException e) {
                             this.initialize();
                             try {
-                                controllerBiglietteria.annullaTicket(proiezione, posto, fila, "no");
+                                postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,id);
+                                postoSuCuiLavorare = ( (String[]) postiRitornati.getIndex(0));
                             } catch (Exception ex) {
+                                throw new ServletException("Errore nella comunicazione con il server SimplyTicket", ex);
+                            }
+                        }
+                        catch (Exception e1) {
+                          throw new ServletException("Errore nella comunicazione con il server SimplyTicket", e1);
+                        }
+                        if (postoSuCuiLavorare[2].equals("TRUE")) {
+                            //JOptionPane.showMessageDialog(null,"            PostoOccupato","Errore",JOptionPane.ERROR_MESSAGE);
+//                            throw new ServletException("posto gia' occupato");
+                        }
+                        else {
+                          if (tipoBiglietto.equalsIgnoreCase("intero")) {
+                            ridotto=0;
+                          }
+                          else {
+                            intero=0;
+                          }
+                          try {
+                            controllerBiglietteria.emettiTicket(proiezione, fila, id, "no",intero,ridotto,IDAddetto);
+                          }
+                          catch (java.rmi.ConnectException e) {
+                              this.initialize();
+                              try {
+                                  controllerBiglietteria.emettiTicket(proiezione, fila, id, "no",intero,ridotto,IDAddetto);
+                              } catch (Exception ex) {
+                                  throw new ServletException("Errore Emissione Ticket", ex);
+                              }
+                          }
+                          catch (Exception ex) {
+                              throw new ServletException("Errore Emissione Ticket", ex);
+                          }
+                        }
+                    }
+                    else {
+                        throw new ServletException("Non hai selezionato alcun posto");
+                    }
+                }
+                JSONArray rimborso = (JSONArray)lista.get("rimborsa");
+                for (Iterator i = rimborso.iterator(); i.hasNext();) {
+                    posto = (JSONObject)i.next();
+                    posto=(JSONObject) posto.get("posto");
+                    fila=Integer.parseInt((String)(posto.get("fila")));
+                    id=Integer.parseInt((String)(posto.get("id")));
+//                    out.println("Rimborso: "+posto.get("proiezione")+" "+posto.get("id")+" "+posto.get("fila")+"<br>\n");
+                    if (id>0 && fila>0) {
+                        Collezione postiRitornati=null;
+                        String[] postoSuCuiLavorare=null;
+                        try {
+                            postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,id);
+                            postoSuCuiLavorare = ( (String[]) postiRitornati.getIndex(0));
+                        }
+                        catch (java.rmi.ConnectException e) {
+                            this.initialize();
+                            try {
+                                postiRitornati=controllerBiglietteria.leggiPosti(proiezione,fila,id);
+                                postoSuCuiLavorare = ( (String[]) postiRitornati.getIndex(0));
+                            } catch (Exception ex) {
+                                throw new ServletException("Errore nella comunicazione con il server SimplyTicket", ex);
+                            }
+                        }
+                        catch (Exception e1) {
+                            throw new ServletException("Errore nella comunicazione con il server SimplyTicket", e1);
+                        }
+                        if (postoSuCuiLavorare[2].equals("FALSE")) {
+                                    //JOptionPane.showMessageDialog(null,"            PostoOccupato","Errore",JOptionPane.ERROR_MESSAGE);
+//                                    throw new ServletException("Posto già rimborsato");
+                        }
+                        else {
+                            if (tipoBiglietto.equalsIgnoreCase("intero")) {
+                                ridotto=0;
+                            }
+                            else {
+                                intero=0;
+                            }
+                            try {
+                                controllerBiglietteria.annullaTicket(proiezione, id, fila, "no");
+                            }
+                            catch (java.rmi.ConnectException e) {
+                                this.initialize();
+                                try {
+                                    controllerBiglietteria.annullaTicket(proiezione, id, fila, "no");
+                                } catch (Exception ex) {
+                                    throw new ServletException("Errore Emissione Ticket", ex);
+                                }
+                            }
+                            catch (Exception ex) {
                                 throw new ServletException("Errore Emissione Ticket", ex);
                             }
                         }
-                        catch (Exception ex) {
-                            throw new ServletException("Errore Emissione Ticket", ex);
-                        }
+                  }
+                    else {
+                       throw new ServletException("Non hai selezionato alcun posto");
                     }
                 }
-                else {
-                   throw new ServletException("Non hai selezionato alcun posto");
-                }
+            } catch (ParseException ex) {
+//                    Logger.getLogger(prenotaPosti2.class.getName()).log(Level.SEVERE, null, ex);
+                throw new ServletException("Errore json",ex);
             }
         }
         String locandinaPath=request.getParameter("locandina"+proiezione);
@@ -225,7 +238,7 @@ public class prenotaPosti extends HttpServlet {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -236,9 +249,9 @@ public class prenotaPosti extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -251,7 +264,7 @@ public class prenotaPosti extends HttpServlet {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
      * @return a String containing servlet description
      */
